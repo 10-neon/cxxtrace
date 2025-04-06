35911b704f35b0e2581da6b47a52ed2df6e12641
dongyilong <dongyilong@bytedance.com>
2025-04-06 16:43:16 +0800

    fix: ci error

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,2 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+#include "cxxtrace/cxxtrace.hpp"
+#include "catch2/catch.hpp"
+
+using namespace cxxtrace;
+
+TEST_CASE( "add_one", "" ){
+    REQUIRE(1 + 1 == 2);
+}
