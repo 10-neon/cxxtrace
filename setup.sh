1db7441e87ced7448b4826c61498967edccf1a3f
dongyilong <dongyilong@bytedance.com>
2025-04-27 05:54:09 +0000

    docs: update doc

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 git init
 git branch -M main
 # install pre-commit hooks
-pip install pre-commit clang-format
-#
+pip install pre-commit commitizen clang-format mkdocs mkdocs-material mkdoxy
+apt-get update && apt-get install -y build-essential libtool autoconf unzip wget ninja-build doxygen
+
+cz init
+pre-commit install
